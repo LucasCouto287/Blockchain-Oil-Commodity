8a6fd9b45b4bce59483606a6841a21a8a7d00268
Nrup R Patel <nruppatel133@gmail.com>
2019-11-24 09:15:41 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 const DIFFICULTY = 3;
 //This difficulty is the amount of leading zeroes needed in the hash
 //The nonce value is the number of attempts completed by the proof of work until the difficulty requirement is satisfied
+const MINE_RATE = 3000;
