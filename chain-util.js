b11d41ffdb2e78fd17d185030450160fd4397e5c
Nrup R Patel <nruppatel133@gmail.com>
2019-11-24 09:18:24 -0500

    Update chain-util.js

--- chain-util.js
+++ chain-util.js
@@ -2,3 +2,8 @@ const EC = require('elliptic').ec;
 const SHA256 = require('crypto-js/sha256');
 const uuidV1 = require('uuid/v1');
 const ec = new EC('secp256k1');
+
+class ChainUtil {
+    static genKeyPair() {
+        return ec.genKeyPair();
+    }
