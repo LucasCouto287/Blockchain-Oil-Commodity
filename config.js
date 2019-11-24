3a6e054fc36e4cf6d52cace57a307527b4e92967
Nrup R Patel <nruppatel133@gmail.com>
2019-11-24 09:15:29 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,3 @@
 const DIFFICULTY = 3;
+//This difficulty is the amount of leading zeroes needed in the hash
+//The nonce value is the number of attempts completed by the proof of work until the difficulty requirement is satisfied
