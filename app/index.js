0b7936ddba40d450378b1c0e071fa0deb1b49ed0
Nrup R Patel <nruppatel133@gmail.com>
2019-11-27 09:07:16 -0500

    Update index.js

--- app/index.js
+++ app/index.js
@@ -4,3 +4,6 @@ const Blockchain = require('../blockchain');
 const P2pServer = require('./p2p-server');
 const Wallet = require('../wallet');
 const TransactionPool = require('../wallet/transaction-pool');
+const Miner = require('./miner');
+
+const HTTP_PORT = process.env.HTTP_PORT || 3001;
