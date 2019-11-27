73bf6ca69804fbed221a57335496b7f19995c601
Nrup R Patel <nruppatel133@gmail.com>
2019-11-27 09:07:04 -0500

    Update index.js

--- app/index.js
+++ app/index.js
@@ -2,3 +2,5 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const Blockchain = require('../blockchain');
 const P2pServer = require('./p2p-server');
+const Wallet = require('../wallet');
+const TransactionPool = require('../wallet/transaction-pool');
