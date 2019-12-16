e12ffc4fe04e73d4407d03a6a509b5234c878590
Nrup R Patel <nruppatel133@gmail.com>
2019-12-16 12:54:11 -0500

    Update p2pserver.js

--- app/p2pserver.js
+++ app/p2pserver.js
@@ -2,3 +2,6 @@ const Websocket = require('ws');
 
 const P2P_PORT = process.env.P2P_PORT || 5001;
 const peers = process.env.PEERS ? process.env.PEERS.split(',') : [];
+
+//These are the tree messages the server handles
+const MESSAGE_TYPES = {
