4d33a1d5454a2e3544c6c64e6ef7d3b4b74680cf
Nrup R Patel <nruppatel133@gmail.com>
2019-12-18 18:13:42 -0500

    Update miner.js

--- app/miner.js
+++ app/miner.js
@@ -4,3 +4,8 @@ const Transaction = require('../wallet/transaction');
 class Miner {
 
     constructor(blockchain, transactionPool, wallet, p2pServer){
+  this.blockchain = blockchain;
+        this.transactionPool = transactionPool;
+        this.wallet = wallet;
+        this.p2pServer = p2pServer;
+    }
