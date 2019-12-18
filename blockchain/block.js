ca42809c5f62bc6f696bb87cfd5f799ce5a4e5d2
Nrup R Patel <nruppatel133@gmail.com>
2019-12-18 18:21:05 -0500

    Update block.js

--- blockchain/block.js
+++ blockchain/block.js
@@ -2,3 +2,17 @@ const ChainUtil = require('../chain-util');
 const { DIFFICULTY, MINE_RATE } = require('../config');
 class Block {
   constructor(timestamp, lastHash, hash, data, nonce, difficulty) {
+
+    this.timestamp = timestamp; 
+   
+    this.lastHash = lastHash; 
+  
+    this.hash = hash; 
+    
+    this.data = data; 
+    
+    this.nonce = nonce;
+
+    this.difficulty = difficulty || DIFFICULTY;
+ 
+  }
