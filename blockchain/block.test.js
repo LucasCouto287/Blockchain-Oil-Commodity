11724911077b898f0e84d46b89439d1d496facae
Nrup R Patel <nruppatel133@gmail.com>
2019-12-20 15:56:46 -0700

    Update block.test.js

--- blockchain/block.test.js
+++ blockchain/block.test.js
@@ -13,6 +13,7 @@ data = 'bar';
  it('sets the `data` to match the input', () => {
         expect(block.data).toEqual(data);
     });
+ 
  it('sets the `lastHash` to match the hash of the last block', () => {
         expect(block.lastHash).toEqual(lastBlock.hash);
     });
