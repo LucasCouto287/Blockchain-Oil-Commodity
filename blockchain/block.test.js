6956206781c3046c3d7162985561667839a0ac61
Nrup R Patel <nruppatel133@gmail.com>
2019-12-20 15:55:34 -0700

    Update block.test.js

--- blockchain/block.test.js
+++ blockchain/block.test.js
@@ -16,3 +16,6 @@ data = 'bar';
  it('sets the `lastHash` to match the hash of the last block', () => {
         expect(block.lastHash).toEqual(lastBlock.hash);
     });
+it('generates a hash that matches the difficulty', () => {
+        expect(block.hash.substring(0, block.difficulty)).toEqual('0'.repeat(block.difficulty));
+    });
