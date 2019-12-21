1e46eef759944f81bdfabf5bf2d090351b599abd
Nrup R Patel <nruppatel133@gmail.com>
2019-12-21 16:33:50 -0700

    P2P 🔢

--- app/p2pserver.js
+++ app/p2pserver.js
@@ -1,89 +1,80 @@
 const Websocket = require('ws');
-
 const P2P_PORT = process.env.P2P_PORT || 5001;
 const peers = process.env.PEERS ? process.env.PEERS.split(',') : [];
-
 //These are the tree messages the server handles
 const MESSAGE_TYPES = {
- chain: "CHAIN",
-transaction: "TRANSACTION",
-    clear_transactions: "CLEAR_TRANSACTIONS"
+	chain: "CHAIN",
+	transaction: "TRANSACTION",
+	clear_transactions: "CLEAR_TRANSACTIONS"
 }
-class P2pServer { 
-    constructor(blockchain, transactionPool){
- this.blockchain = blockchain;
-        this.transactionPool = transactionPool;
-  this.sockets = [];
-    }
-
-  listen() {
-        const server = new Websocket.Server({ port: P2P_PORT });
- server.on('connection', socket => this.connectSocket(socket));
-    this.connectToPeers();
-
-        console.log(`Listening for peer to peer connections on: ${P2P_PORT}`);
-    }
-
- //As peers connect, they get their own socket and then this socket is connected
-    connectToPeers() {
-  peers.forEach(peer => {
-        const socket = new Websocket(peer);
-   socket.on('open', () => this.connectSocket(socket));
-        });
-    }
-
- //Conecting the socket adds it onto an array of the users and sends the user the blockchain
-    connectSocket(socket) {
- this.sockets.push(socket);
-        console.log('Socket connected');
-  this.messageHandler(socket);
-
-        this.sendChain(socket);
-    }
-
- //This method executes the appropriate action for the respective message received
-    messageHandler(socket){
- socket.on('message', message => {
-            const data = JSON.parse(message);
-            switch(data.type) {
-  case MESSAGE_TYPES.chain:
-                  this.blockchain.replaceChain(data.chain);
-                  break;
-                case MESSAGE_TYPES.transaction:
-                  this.transactionPool.updateOrAddTransaction(data.transaction);
-                  break;
-    case MESSAGE_TYPES.clear_transactions:
-                    this.transactionPool.clear();
-                    break;
-            }
-        });
-    }
-
-    //This method sends the blockchain through a socket and gets it to new users
-    sendChain(socket){
-  socket.send(JSON.stringify({
-            type: MESSAGE_TYPES.chain,
-            chain: this.blockchain.chain
-        }));
-    }
- sendTransaction(socket, transaction){
-        socket.send(JSON.stringify({
-    type: MESSAGE_TYPES.transaction,
-            transaction
-        }));
-    }
-   syncChains() {
-        this.sockets.forEach(socket => this.sendChain(socket));
-    }
-    broadcastTransaction(transaction){
-        this.sockets.forEach(socket => this.sendTransaction(socket, transaction));
-    }
-   broadcastClearTransactions() {
-        this.sockets.forEach(socket => socket.send(JSON.stringify({
-            type: MESSAGE_TYPES.clear_transactions
-        })));
-    }
+class P2pServer {
+	constructor(blockchain, transactionPool) {
+		this.blockchain = blockchain;
+		this.transactionPool = transactionPool;
+		this.sockets = [];
+	}
+	listen() {
+		const server = new Websocket.Server({
+			port: P2P_PORT
+		});
+		server.on('connection', socket => this.connectSocket(socket));
+		this.connectToPeers();
+		console.log(`Listening for peer to peer connections on: ${P2P_PORT}`);
+	}
+	//As peers connect, they get their own socket and then this socket is connected
+	connectToPeers() {
+		peers.forEach(peer => {
+			const socket = new Websocket(peer);
+			socket.on('open', () => this.connectSocket(socket));
+		});
+	}
+	//Conecting the socket adds it onto an array of the users and sends the user the blockchain
+	connectSocket(socket) {
+		this.sockets.push(socket);
+		console.log('Socket connected');
+		this.messageHandler(socket);
+		this.sendChain(socket);
+	}
+	//This method executes the appropriate action for the respective message received
+	messageHandler(socket) {
+		socket.on('message', message => {
+			const data = JSON.parse(message);
+			switch (data.type) {
+				case MESSAGE_TYPES.chain:
+					this.blockchain.replaceChain(data.chain);
+					break;
+				case MESSAGE_TYPES.transaction:
+					this.transactionPool.updateOrAddTransaction(data.transaction);
+					break;
+				case MESSAGE_TYPES.clear_transactions:
+					this.transactionPool.clear();
+					break;
+			}
+		});
+	}
+	//This method sends the blockchain through a socket and gets it to new users
+	sendChain(socket) {
+		socket.send(JSON.stringify({
+			type: MESSAGE_TYPES.chain,
+			chain: this.blockchain.chain
+		}));
+	}
+	sendTransaction(socket, transaction) {
+		socket.send(JSON.stringify({
+			type: MESSAGE_TYPES.transaction,
+			transaction
+		}));
+	}
+	syncChains() {
+		this.sockets.forEach(socket => this.sendChain(socket));
+	}
+	broadcastTransaction(transaction) {
+		this.sockets.forEach(socket => this.sendTransaction(socket, transaction));
+	}
+	broadcastClearTransactions() {
+		this.sockets.forEach(socket => socket.send(JSON.stringify({
+			type: MESSAGE_TYPES.clear_transactions
+		})));
+	}
 }
-
 module.exports = P2pServer;
-
