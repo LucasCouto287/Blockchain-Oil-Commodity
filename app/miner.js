ee9eb3ac7cfb91a135cd052ddca1bafe1dd17922
Nrup R Patel <nruppatel133@gmail.com>
2019-12-21 16:32:12 -0700

    Miners ⛏️

--- app/miner.js
+++ app/miner.js
@@ -1,32 +1,27 @@
 const Wallet = require('../wallet');
 const Transaction = require('../wallet/transaction');
-
 class Miner {
-
-    constructor(blockchain, transactionPool, wallet, p2pServer){
-  this.blockchain = blockchain;
-        this.transactionPool = transactionPool;
-        this.wallet = wallet;
-        this.p2pServer = p2pServer;
-    }
-    // Mines a new transaction on the blockchain by:
-    //Validating transactions on the transaction pool
-    //Rewarding the miner
-    //Creating new block on the blockchain consisting of the newly validated transactions
-    //Synchronizes blockchains between all other peers
-    //Clears transaction pool
-    //Broadcasts to every miner to clear their transaction pool
-
-     mine() {
-        const validTransactions = this.transactionPool.validTransactions();
- validTransactions.push(
-  Transaction.rewardTransaction(this.wallet, Wallet.blockchainWallet())
-        );
-  const block = this.blockchain.addBlock(validTransactions);
-  this.p2pServer.syncChains();
-  this.transactionPool.clear();
- this.p2pServer.broadcastClearTransactions();
- return block;
-    }
+	constructor(blockchain, transactionPool, wallet, p2pServer) {
+		this.blockchain = blockchain;
+		this.transactionPool = transactionPool;
+		this.wallet = wallet;
+		this.p2pServer = p2pServer;
+	}
+	// Mines a new transaction on the blockchain by:
+	//Validating transactions on the transaction pool
+	//Rewarding the miner
+	//Creating new block on the blockchain consisting of the newly validated transactions
+	//Synchronizes blockchains between all other peers
+	//Clears transaction pool
+	//Broadcasts to every miner to clear their transaction pool
+	mine() {
+		const validTransactions = this.transactionPool.validTransactions();
+		validTransactions.push(Transaction.rewardTransaction(this.wallet, Wallet.blockchainWallet()));
+		const block = this.blockchain.addBlock(validTransactions);
+		this.p2pServer.syncChains();
+		this.transactionPool.clear();
+		this.p2pServer.broadcastClearTransactions();
+		return block;
+	}
 }
 module.exports = Miner;
