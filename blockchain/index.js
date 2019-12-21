d95f3a2793128b10f3d3bf7b31f9b8e36a70b18f
Nrup R Patel <nruppatel133@gmail.com>
2019-12-21 16:05:41 -0700

    Update index.js

--- blockchain/index.js
+++ blockchain/index.js
@@ -14,3 +14,5 @@ addBlock(data){
         isValidChain(chain) {
 if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) return false;
 for (let i = 1; i<chain.length; i++) {
+const block = chain[i];
+            const lastBlock = chain[i-1];
