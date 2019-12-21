aad031a19c3a9536b73e4001471ad37c5e8b953e
Nrup R Patel <nruppatel133@gmail.com>
2019-12-21 16:20:03 -0700

    Block 1🍫

--- blockchain/block.js
+++ blockchain/block.js
@@ -1,68 +1,66 @@
 const ChainUtil = require('../chain-util');
-const { DIFFICULTY, MINE_RATE } = require('../config');
+const {
+	DIFFICULTY,
+	MINE_RATE
+} = require('../config');
 class Block {
-  constructor(timestamp, lastHash, hash, data, nonce, difficulty) {
-
-    this.timestamp = timestamp; 
-   
-    this.lastHash = lastHash; 
-  
-    this.hash = hash; 
-    
-    this.data = data; 
-    
-    this.nonce = nonce;
-
-    this.difficulty = difficulty || DIFFICULTY;
- 
-  }
-
-  
-  toString() {
-      return `Block - 
+	constructor(timestamp, lastHash, hash, data, nonce, difficulty) {
+		this.timestamp = timestamp;
+		this.lastHash = lastHash;
+		this.hash = hash;
+		this.data = data;
+		this.nonce = nonce;
+		this.difficulty = difficulty || DIFFICULTY;
+	}
+	toString() {
+		return `Block - 
  Timestamp : ${this.timestamp}
       Last Hash : ${this.lastHash.substring(0,10)}
  Hash      : ${this.hash.substring(0,10)}
       Nonce     : ${this.nonce}
 Difficulty: ${this.difficulty}
       Data      : ${this.data}`;
-  }
-
-   static genesis() {
-   return new this('Genesis time', '-----', 'theGenesisBlock', [], 0, DIFFICULTY);
-  }
-
-    static mineBlock(lastBlock, data) {
- let hash, timestamp; 
-      const lastHash = lastBlock.hash;
-      let { difficulty } = lastBlock;
-      let nonce = 0;
-      
-       //Proof of Work is established here
-      do {
-        nonce++;
-         timestamp = Date.now();
-        difficulty = Block.adjustDifficulty(lastBlock, timestamp);
-        hash = Block.hash(timestamp, lastHash, data, nonce, difficulty);
-      } while (hash.substring(0, difficulty) !== '0'.repeat(difficulty));
-   return new this(timestamp, lastHash, hash, data, nonce, difficulty);
-  }
-static hash(timestamp, lastHash, data, nonce, difficulty) {
-return ChainUtil.hash(`${timestamp}${lastHash}${data}${nonce}${difficulty}`).toString();
-  }
-
-  static blockHash(block) {
-    const {timestamp, lastHash, data, nonce, difficulty} = block;
- return Block.hash(timestamp, lastHash, data, nonce, difficulty);
-  }
-
-  //This method checks the timestamp from the previous block to see if the time passed is greater than or less than the mine rate
-  //and then accordingly decreases or increases the difficulty
- static adjustDifficulty(lastBlock, currentTime) {
- let { difficulty } = lastBlock; 
- difficulty = lastBlock.timestamp + MINE_RATE > currentTime ? difficulty + 1 : difficulty - 1;
-   return difficulty;
-  }
+	}
+	static genesis() {
+		return new this('Genesis time', '-----', 'theGenesisBlock', [], 0, DIFFICULTY);
+	}
+	static mineBlock(lastBlock, data) {
+		let hash, timestamp;
+		const lastHash = lastBlock.hash;
+		let {
+			difficulty
+		} = lastBlock;
+		let nonce = 0;
+		//Proof of Work is established here
+		do {
+			nonce++;
+			timestamp = Date.now();
+			difficulty = Block.adjustDifficulty(lastBlock, timestamp);
+			hash = Block.hash(timestamp, lastHash, data, nonce, difficulty);
+		} while (hash.substring(0, difficulty) !== '0'.repeat(difficulty));
+		return new this(timestamp, lastHash, hash, data, nonce, difficulty);
+	}
+	static hash(timestamp, lastHash, data, nonce, difficulty) {
+		return ChainUtil.hash(`${timestamp}${lastHash}${data}${nonce}${difficulty}`).toString();
+	}
+	static blockHash(block) {
+		const {
+			timestamp,
+			lastHash,
+			data,
+			nonce,
+			difficulty
+		} = block;
+		return Block.hash(timestamp, lastHash, data, nonce, difficulty);
+	}
+	//This method checks the timestamp from the previous block to see if the time passed is greater than or less than the mine rate
+	//and then accordingly decreases or increases the difficulty
+	static adjustDifficulty(lastBlock, currentTime) {
+		let {
+			difficulty
+		} = lastBlock;
+		difficulty = lastBlock.timestamp + MINE_RATE > currentTime ? difficulty + 1 : difficulty - 1;
+		return difficulty;
+	}
 }
-
-module.exports = Block; 
+module.exports = Block;
