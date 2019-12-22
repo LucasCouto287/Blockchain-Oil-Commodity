edb1a209e800d95074695b42e48c969086b09118
Nrup R Patel <nruppatel133@gmail.com>
2019-12-22 02:05:38 -0700

    Update index.js

--- wallet/index.js
+++ wallet/index.js
@@ -4,3 +4,7 @@ const {INITIAL_BALANCE } = require('../config');
 
 class Wallet {
     constructor() {
+        this.balance = INITIAL_BALANCE; 
+        this.keyPair = ChainUtil.genKeyPair(); 
+        this.publicKey = this.keyPair.getPublic().encode('hex'); 
+    }
