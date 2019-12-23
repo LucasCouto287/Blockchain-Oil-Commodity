08b68c885350b65b057bf2bc20b0d4810c111816
Nrup R Patel <nruppatel133@gmail.com>
2019-12-23 10:36:01 -1000

    Update index.test.js

--- wallet/index.test.js
+++ wallet/index.test.js
@@ -2,3 +2,6 @@ const Wallet = require('./index');
 const TransactionPool = require('./transaction-pool');
 const Blockchain = require('../blockchain');
 const { INITIAL_BALANCE } = require('../config');
+
+describe('Wallet', () => {
+  let wallet, tp, bc;
