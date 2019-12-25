0d1dfcca7f95f484cf91a3a075064bb85cf7a019
Nrup R Patel <nruppatel133@gmail.com>
2019-12-25 10:12:54 -1000

    Update index.test.js

--- wallet/index.test.js
+++ wallet/index.test.js
@@ -9,7 +9,7 @@ describe('Wallet', () => {
 			beforeEach(() => {
 				wallet = new Wallet();
 				tp = new TransactionPool();
-				tc = new Blockchain();
+				bc = new Blockchain();
 			});
 			describe('creating a transaction', () => {
 				let transaction, sendAmount, recipient;
