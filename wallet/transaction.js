77fe13bb24e78e162b28edbe44b2cefb8684289c
Nrup R Patel <nruppatel133@gmail.com>
2019-12-30 18:01:52 -0700

    Update transaction.js

--- wallet/transaction.js
+++ wallet/transaction.js
@@ -1,2 +1,9 @@
 const ChainUtil = require('../chain-util');
 const { MINING_REWARD} = require('../config');
+
+class Transaction {
+    constructor() {
+        this.id = ChainUtil.id();
+        this.input = null;
+        this.outputs = [];
+    }
