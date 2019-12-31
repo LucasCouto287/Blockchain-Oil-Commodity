3e775a21de64defa825b720c6e72dc8f637b02b4
Nrup R Patel <nruppatel133@gmail.com>
2019-12-30 17:48:14 -0700

    Update transaction-pool.test.js

--- wallet/transaction-pool.test.js
+++ wallet/transaction-pool.test.js
@@ -6,3 +6,12 @@ const Blockchain = require('../blockchain');
 
 describe('TransactionPool', () => {
     let tp, wallet, transaction, bc;
+
+   
+ beforeEach(() => {
+      tp = new TransactionPool();
+      wallet = new Wallet();
+      bc = new Blockchain();
+      transaction = wallet.createTransaction('r4nd-4dr355', 30, bc, tp);
+    });
+  
