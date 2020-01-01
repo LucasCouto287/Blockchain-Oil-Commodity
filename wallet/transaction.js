a737dc6ae7e99577dfd0c9cd3d0be9198a2c0d46
Nrup R Patel <nruppatel133@gmail.com>
2019-12-31 22:51:27 -0700

    Trans exports test
    Next pull request

--- wallet/transaction.js
+++ wallet/transaction.js
@@ -22,53 +22,42 @@ class Transaction {
 		Transaction.signTransaction(this, senderWallet);
 		return this;
 	}
-
-	
-	
-    static transactionWithOutputs(senderWallet, outputs) {
-        const transaction = new this();
-        transaction.outputs.push(...outputs);
-        Transaction.signTransaction(transaction, senderWallet);
-        return transaction;
-    }
-
-	
+	static transactionWithOutputs(senderWallet, outputs) {
+		const transaction = new this();
+		transaction.outputs.push(...outputs);
+		Transaction.signTransaction(transaction, senderWallet);
+		return transaction;
+	}
 	static newTransaction(senderWallet, recipient, amount) {
-        if (amount > senderWallet.balance){
-            console.log(`Amount: ${amount} exceeds balance.`);
-            return;
-        }
-
-		
-		 return Transaction.transactionWithOutputs(senderWallet, [
-            { amount: senderWallet.balance - amount, address: senderWallet.publicKey },
-            { amount, address: recipient }
-        ]);
-    }
-
+		if (amount > senderWallet.balance) {
+			console.log(`Amount: ${amount} exceeds balance.`);
+			return;
+		}
+		return Transaction.transactionWithOutputs(senderWallet, [{
+			amount: senderWallet.balance - amount,
+			address: senderWallet.publicKey
+		}, {
+			amount,
+			address: recipient
+		}]);
+	}
 	static rewardTransaction(minerWallet, blockchainWallet) {
-        return Transaction.transactionWithOutputs(blockchainWallet, [{
-	            amount: MINING_REWARD, address: minerWallet.publicKey
-        }]);
-    }
-    
-    static signTransaction(transaction, senderWallet) {
-transaction.input = { 
-            timestamp: Date.now(),
-     amount: senderWallet.balance,
-            address: senderWallet.publicKey,
-            signature: senderWallet.sign(ChainUtil.hash(transaction.outputs))
-        }
-    }
-
-	 //This is used by the recipient to verify the transaction
-    static verifyTransaction(transaction) {
-	        return ChainUtil.verifySignature(
-  transaction.input.address,
-            transaction.input.signature,
-            ChainUtil.hash(transaction.outputs)
-        );
-    }
+		return Transaction.transactionWithOutputs(blockchainWallet, [{
+			amount: MINING_REWARD,
+			address: minerWallet.publicKey
+		}]);
+	}
+	static signTransaction(transaction, senderWallet) {
+		transaction.input = {
+			timestamp: Date.now(),
+			amount: senderWallet.balance,
+			address: senderWallet.publicKey,
+			signature: senderWallet.sign(ChainUtil.hash(transaction.outputs))
+		}
+	}
+	//This is used by the recipient to verify the transaction
+	static verifyTransaction(transaction) {
+		return ChainUtil.verifySignature(transaction.input.address, transaction.input.signature, ChainUtil.hash(transaction.outputs));
+	}
 }
-	
- module.exports = Transaction;
+module.exports = Transaction;
