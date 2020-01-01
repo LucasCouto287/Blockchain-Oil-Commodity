6e2330bb81fbb2428fb11e22360548d9d6b2db86
Nrup R Patel <nruppatel133@gmail.com>
2019-12-31 22:43:24 -0700

    Update transaction.test.js

--- wallet/transaction.test.js
+++ wallet/transaction.test.js
@@ -1,3 +1,7 @@
 const Transaction = require('./transaction');
 const Wallet = require('./index');
 const { MINING_REWARD } = require('../config');
+
+
+describe('Transaction', () => {
+    let transaction, wallet, recipient, amount;
